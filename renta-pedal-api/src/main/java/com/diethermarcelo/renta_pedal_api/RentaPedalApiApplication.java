5976ba1c3640d7409c3a72fc2f45138ec3b53073
Diether Marcelo <miguelitossss199@gmail.com>
2024-06-16 14:57:44 +0800

    Initial Commit

---
+++ renta-pedal-api/src/main/java/com/diethermarcelo/renta_pedal_api/RentaPedalApiApplication.java
@@ -0,0 +1,13 @@
+package com.diethermarcelo.renta_pedal_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentaPedalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentaPedalApiApplication.class, args);
+	}
+
+}
---
+++ renta-pedal-api/src/test/java/com/diethermarcelo/renta_pedal_api/RentaPedalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diethermarcelo.renta_pedal_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentaPedalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
